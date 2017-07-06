882350d4f8662bf746b4f72a4aa7a30c4f5f470a
Pedro <pedro.morales@ebury.com>
2017-07-06 18:34:29 +0200

    feat: implemented predict and update (standard & extended) methods for Kalman filter class. compiles ok.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -19,21 +19,47 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
-    * predict the state
+    * Predict the state
   */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Kalman Filter equations
+    * Update the state with Standard Kalman Filter equations
+    * given the measurment vector @param z
   */
+  
+  MatrixXd I = MatrixXd::Identity(z.size(), z.size());
+  
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_; 
+  MatrixXd K = P_ * Ht * S.inverse();
+  x_ += K * y;
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
+    * Update the state with Extended Kalman Filter equations
+    * given the measurement vector @param z
   */
+  
+  MatrixXd I = MatrixXd::Identity(z.size(), z.size());
+  
+  // components of state vector in polar coordinates
+  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  float phi = atan2(x_(1), x_(0));
+  float rho_dot = (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
+  VectorXd hx(3);
+  hx << rho, phi, rho_dot;
+  
+  VectorXd y = z - hx;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_; 
+  MatrixXd K = P_ * Ht * S.inverse();
+  x_ += K * y;
+  P_ = (I - K * H_) * P_;
 }
